f3deb5913bb2d7c5ceddbdf05ac15df920259a62
Joachim Pouderoux <joachim.pouderoux@kitware.com>
2013-12-14 13:15:50 +0100

    Add LUT support
    Change-Id: Iaf46066797e12583600a0176ec665030eea80f56

--- Charts/Core/vtkPlotBox.h
+++ Charts/Core/vtkPlotBox.h
@@ -67,14 +67,6 @@ public:
   virtual void GetBounds(double bounds[4]);
 
   // Description:
-  // Set the selection criteria on the given axis in normalized space (0.0 - 1.0).
-  bool SetSelectionRange(int Axis, float low, float high);
-
-  // Description:
-  // Reset the selection criteria for the chart.
-  bool ResetSelectionRange();
-
-  // Description:
   // This is a convenience function to set the input table.
   virtual void SetInputData(vtkTable *table);
   virtual void SetInputData(vtkTable *table, const vtkStdString&,
@@ -108,20 +100,9 @@ public:
   vtkBooleanMacro(ScalarVisibility,int);
 
   // Description:
-  // When ScalarMode is set to UsePointFieldData or UseCellFieldData,
-  // you can specify which array to use for coloring using these methods.
-  // The lookup table will decide how to convert vectors to colors.
-  void SelectColorArray(vtkIdType arrayNum);
-  void SelectColorArray(const vtkStdString &arrayName);
-
-  // Description:
   // Get/Set the width of boxes.
   vtkGetMacro(BoxWidth, float);
   vtkSetMacro(BoxWidth, float);
-
-  // Description:
-  // Get the array name to color by.
-  vtkStdString GetColorArrayName();
 
 //BTX
 protected:
@@ -146,9 +127,7 @@ protected:
   // Description:
   // Lookup Table for coloring points by scalar value
   vtkScalarsToColors *LookupTable;
-  vtkUnsignedCharArray *Colors;
   int ScalarVisibility;
-  vtkStdString ColorArrayName;
 
 private:
   vtkPlotBox(const vtkPlotBox &); // Not implemented.
