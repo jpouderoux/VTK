2193e87739257d92d96968a0d61d8364e029a9e2
Joachim Pouderoux <joachim.pouderoux@kitware.com>
2013-12-14 13:15:50 +0100

    Add LUT support
    Change-Id: Iaf46066797e12583600a0176ec665030eea80f56

--- Charts/Core/vtkPlotBox.cxx
+++ Charts/Core/vtkPlotBox.cxx
@@ -65,7 +65,6 @@ vtkPlotBox::vtkPlotBox()
   this->Pen->SetColor(255, 0, 0, 25);
   this->BoxWidth = 20.;
   this->LookupTable = 0;
-  this->Colors = 0;
   this->ScalarVisibility = 0;
   this->TooltipDefaultLabelFormat = "%y";
 }
@@ -78,10 +77,6 @@ vtkPlotBox::~vtkPlotBox()
   if (this->LookupTable)
     {
     this->LookupTable->UnRegister(this);
-    }
-  if (this->Colors)
-    {
-    this->Colors->UnRegister(this);
     }
 }
 
@@ -123,25 +118,14 @@ bool vtkPlotBox::Paint(vtkContext2D *painter)
 
   size_t cols = this->Storage->size();
 
-  vtkIdType selection = 0;
-  vtkIdType id = 0;
-  vtkIdType selectionSize = 0;
-  if (this->Selection)
-    {
-    selectionSize = this->Selection->GetNumberOfTuples();
-    if (selectionSize)
-      {
-      this->Selection->GetTupleValue(selection, &id);
-      }
-    }
-
-    // Update the axis positions
   vtkChartBox *parent = vtkChartBox::SafeDownCast(this->Parent);
 
   for (int i = 0; i < cols; i++)
     {
+    double rgb[3];
+    this->LookupTable->GetColor(i, rgb);
     vtkNew<vtkBrush> brush;
-    brush->SetColor(this->Pen->GetColor());
+    brush->SetColor(rgb[0] * 255., rgb[1] * 255, rgb[2] * 255, 255);
     vtkNew<vtkPen> blackPen;
     blackPen->SetWidth(this->Pen->GetWidth());
     blackPen->SetColor(0, 0, 0, 128);
@@ -149,8 +133,8 @@ bool vtkPlotBox::Paint(vtkContext2D *painter)
 
     if (parent->GetSelectedColumn() == i)
       {
-      unsigned char* col = this->Pen->GetColor();
-      brush->SetColor(col[0]^255,col[1]^255,col[2]^255,255);
+      unsigned char* col = brush->GetColor();
+      brush->SetColor(col[0]^255, col[1]^255, col[2]^255, 255);
       }
 
     painter->ApplyPen(blackPen.GetPointer());
@@ -195,124 +179,19 @@ bool vtkPlotBox::Paint(vtkContext2D *painter)
     painter->DrawLine(xneg, q[2], xpos, q[2]);
   }
 
-  // Draw all of the lines
-  /*painter->ApplyPen(this->Pen);
-  int ncComps(0);
-  if (this->ScalarVisibility && this->Colors)
-    {
-    ncComps = static_cast<int>(this->Colors->GetNumberOfComponents());
-    }
-  if (this->ScalarVisibility && this->Colors && ncComps == 4)
-    {
-    for (size_t i = 0, nc = 0; i < rows; ++i, nc += ncComps)
-      {
-      for (size_t j = 0; j < cols; ++j)
-        {
-        line[j].Set(this->Storage->AxisPos[j], (*this->Storage)[j][i]);
-        }
-      painter->GetPen()->SetColor(this->Colors->GetPointer(nc));
-      painter->DrawPoly(line[0].GetData(), static_cast<int>(cols));
-      }
-    }
-  else
-    {
-    for (size_t i = 0; i < rows; ++i)
-      {
-      for (size_t j = 0; j < cols; ++j)
-        {
-        line[j].Set(this->Storage->AxisPos[j], (*this->Storage)[j][i]);
-        }
-      painter->DrawPoly(line[0].GetData(), static_cast<int>(cols));
-      }
-    }
-
-  // Now draw the selected lines
-  if (this->Selection)
-    {
-    painter->GetPen()->SetColor(255, 0, 0, 100);
-    for (vtkIdType i = 0; i < this->Selection->GetNumberOfTuples(); ++i)
-      {
-      for (size_t j = 0; j < cols; ++j)
-        {
-        this->Selection->GetTupleValue(i, &id);
-        line[j].Set(this->Storage->AxisPos[j], (*this->Storage)[j][id]);
-        }
-      painter->DrawPoly(line[0].GetData(), static_cast<int>(cols));
-      }
-    }
-
-  delete[] line;*/
-
   return true;
 }
 
 //-----------------------------------------------------------------------------
 bool vtkPlotBox::PaintLegend(vtkContext2D *painter,
-                                             const vtkRectf& rect, int)
+                             const vtkRectf& rect, int)
 {
-  painter->ApplyPen(this->Pen);
-  painter->DrawLine(rect[0]          , rect[1] + 0.5 * rect[3],
-                    rect[0] + rect[2], rect[1] + 0.5 * rect[3]);
   return true;
 }
 
 //-----------------------------------------------------------------------------
 void vtkPlotBox::GetBounds(double *)
 {
-
-}
-
-//-----------------------------------------------------------------------------
-bool vtkPlotBox::SetSelectionRange(int axis, float low, float high)
-{
-  if (!this->Selection)
-    {
-    return false;
-    }
-  if (this->Storage->SelectionInitialized)
-    {
-    // Further refine the selection that has already been made
-    vtkIdTypeArray *array = vtkIdTypeArray::New();
-    std::vector<double>& col = this->Storage->at(axis);
-    for (vtkIdType i = 0; i < this->Selection->GetNumberOfTuples(); ++i)
-      {
-      vtkIdType id = 0;
-      this->Selection->GetTupleValue(i, &id);
-      if (col[id] >= low && col[id] <= high)
-        {
-        // Remove this point - no longer selected
-        array->InsertNextValue(id);
-        }
-      }
-    this->Selection->DeepCopy(array);
-    array->Delete();
-    }
-  else
-    {
-    // First run - ensure the selection list is empty and build it up
-    std::vector<double>& col = this->Storage->at(axis);
-    for (size_t i = 0; i < col.size(); ++i)
-      {
-      if (col[i] >= low && col[i] <= high)
-        {
-        // Remove this point - no longer selected
-        this->Selection->InsertNextValue(i);
-        }
-      }
-    this->Storage->SelectionInitialized = true;
-    }
-  return true;
-}
-
-//-----------------------------------------------------------------------------
-bool vtkPlotBox::ResetSelectionRange()
-{
-  this->Storage->SelectionInitialized = false;
-  if (this->Selection)
-    {
-    this->Selection->SetNumberOfTuples(0);
-    }
-  return true;
 }
 
 //-----------------------------------------------------------------------------
@@ -326,8 +205,7 @@ void vtkPlotBox::SetInputData(vtkTable* table)
 
   bool updateVisibility = table != this->Data->GetInput();
   this->vtkPlot::SetInputData(table);
-  vtkChartBox *parent =
-      vtkChartBox::SafeDownCast(this->Parent);
+  vtkChartBox *parent = vtkChartBox::SafeDownCast(this->Parent);
 
   if (parent && table && updateVisibility)
     {
@@ -343,22 +221,22 @@ void vtkPlotBox::SetInputData(vtkTable* table)
     // No table, therefore no visible columns
     parent->GetVisibleColumns()->SetNumberOfTuples(0);
     }
+  if (!this->LookupTable)
+    {
+    this->CreateDefaultLookupTable();
+    }
 }
+
 namespace
 {
 // See if the point is within tolerance.
 bool inRange(const vtkVector2f& point, const vtkVector2f& tol,
              const vtkVector2f& current)
 {
-  if (current.GetX() > point.GetX() - tol.GetX() && current.GetX() < point.GetX() + tol.GetX() &&
-      current.GetY() > point.GetY() - tol.GetY() && current.GetY() < point.GetY() + tol.GetY())
-    {
-    return true;
-    }
-  else
-    {
-    return false;
-    }
+  return current.GetX() > point.GetX() - tol.GetX() &&
+         current.GetX() < point.GetX() + tol.GetX() &&
+         current.GetY() > point.GetY() - tol.GetY() &&
+         current.GetY() < point.GetY() + tol.GetY();
 }
 }
 
@@ -434,30 +312,6 @@ bool vtkPlotBox::UpdateTableCache(vtkTable *table)
       }
     }
 
-  // Additions for color mapping
-  if (this->ScalarVisibility && !this->ColorArrayName.empty())
-    {
-    vtkDataArray* c =
-      vtkDataArray::SafeDownCast(table->GetColumnByName(this->ColorArrayName));
-    // TODO: Should add support for categorical coloring & try enum lookup
-    if (c)
-      {
-      if (!this->LookupTable)
-        {
-        this->CreateDefaultLookupTable();
-        }
-      this->Colors = this->LookupTable->MapScalars(c, VTK_COLOR_MODE_MAP_SCALARS, -1);
-      // Consistent register and unregisters
-      this->Colors->Register(this);
-      this->Colors->Delete();
-      }
-    else
-      {
-      this->Colors->UnRegister(this);
-      this->Colors = 0;
-      }
-    }
-
   this->BuildTime.Modified();
   return true;
 }
@@ -501,70 +355,9 @@ void vtkPlotBox::CreateDefaultLookupTable()
   // Consistent Register/UnRegisters.
   this->LookupTable->Register(this);
   this->LookupTable->Delete();
-}
-
-//-----------------------------------------------------------------------------
-void vtkPlotBox::SelectColorArray(const vtkStdString &arrayName)
-{
-  vtkTable *table = this->Data->GetInput();
-  if (!table)
-    {
-    vtkDebugMacro(<< "SelectColorArray called with no input table set.");
-    return;
-    }
-  if (this->ColorArrayName == arrayName)
-    {
-    return;
-    }
-  for (vtkIdType c = 0; c < table->GetNumberOfColumns(); ++c)
-    {
-    if (table->GetColumnName(c) == arrayName)
-      {
-      this->ColorArrayName = arrayName;
-      this->Modified();
-      return;
-      }
-    }
-  vtkDebugMacro(<< "SelectColorArray called with invalid column name.");
-  this->ColorArrayName = "";
-  this->Modified();
-  return;
-}
-
-//-----------------------------------------------------------------------------
-vtkStdString vtkPlotBox::GetColorArrayName()
-{
-  return this->ColorArrayName;
-}
-
-//-----------------------------------------------------------------------------
-void vtkPlotBox::SelectColorArray(vtkIdType arrayNum)
-{
-  vtkTable *table = this->Data->GetInput();
-  if (!table)
-    {
-    vtkDebugMacro(<< "SelectColorArray called with no input table set.");
-    return;
-    }
-  vtkDataArray *col = vtkDataArray::SafeDownCast(table->GetColumn(arrayNum));
-  // TODO: Should add support for categorical coloring & try enum lookup
-  if (!col)
-    {
-    vtkDebugMacro(<< "SelectColorArray called with invalid column index");
-    return;
-    }
-  else
-    {
-    if (this->ColorArrayName == table->GetColumnName(arrayNum))
-      {
-      return;
-      }
-    else
-      {
-      this->ColorArrayName = table->GetColumnName(arrayNum);
-      this->Modified();
-      }
-    }
+  vtkTable *table = this->GetInput();
+  this->LookupTable->SetRange(0, table->GetNumberOfColumns());
+  this->LookupTable->Build();
 }
 
 //-----------------------------------------------------------------------------
